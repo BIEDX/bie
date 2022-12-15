f75bfb538442afed66534711b1733e73c3a45ac7
praispranav <praispranav@gmail.com>
2022-12-15 19:45:52 +0530

    Added News Letter Api

--- src/app/shared/layout/subscribe/subscribe.component.ts
+++ src/app/shared/layout/subscribe/subscribe.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ProviderUserAuthService } from 'src/app/core/providers/auth/provider-user-auth.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-subscribe',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subscribe.component.scss']
 })
 export class SubscribeComponent implements OnInit {
+  @ViewChild('btn', { static: false }) btn: ElementRef<HTMLElement>;
 
-  constructor() { }
+  name: string = '';
+  email: string = '';
+  checked: boolean = false;
+
+  constructor(private userService: ProviderUserAuthService, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.init();
+  }
+
+  init(){
+    this.userService.getUserStorage().subscribe((data)=>{
+      if(!data){
+        setTimeout(()=>{
+          this.btn.nativeElement.click()
+        }, 15000)
+      }
+    });
+  }
+
+  submit(event){
+    event.preventDefault();
+    this.http.post(environment.apiUrl + '/newsletter', { name: this.name, email: this.email}).subscribe((res)=>{
+      console.log("Success")
+      this.name = '';
+      this.email = ''
+    }, (error)=> console.log(error))
+  }
+
+  onChange(){
+    this.checked = !this.checked;
   }
 
 }
