f9a63a0e1d7b0d7d1b650affe51912bd5dc0c710
Pranav Kumar <pranav@scorpius.com.my>
2022-12-15 05:31:01 +0000

    Added Header and Created Components

---
+++ src/app/modules/static-pages/pages/consult-topic/consult-topic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consult-topic',
+  templateUrl: './consult-topic.component.html',
+  styleUrls: ['./consult-topic.component.scss']
+})
+export class ConsultTopicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/static-pages/pages/learn-topic/learn-topic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-learn-topic',
+  templateUrl: './learn-topic.component.html',
+  styleUrls: ['./learn-topic.component.scss']
+})
+export class LearnTopicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
