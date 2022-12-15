43419a6946062173d5d86c05c16053a8211a1202
praispranav <praispranav@gmail.com>
2022-12-15 19:51:45 +0530

    Added Api To Footer

--- src/app/shared/layout/footer/footer.component.ts
+++ src/app/shared/layout/footer/footer.component.ts
@@ -1,15 +1,29 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'el-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  email: string = '';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  submit(event) {
+    event.preventDefault();
+    this.http
+      .post(environment.apiUrl + '/newsletter', { name: '', email: this.email })
+      .subscribe(
+        (res) => {
+
+          console.log('Success', this.email);
+          this.email = '';
+        },
+        (error) => console.log(error)
+      );
   }
-
 }
