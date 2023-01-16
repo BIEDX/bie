0f388c5e796956db1c72f3f30be641ee9bf9a152
unknown <mo.naved0001@gmail.com>
2023-01-17 00:19:33 +0530

    changes done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:'https://biebackend.azurewebsites.net'
+  apiUrl:'https://biebackend.azurewebsites.net',
+  imgUrl: 'https://ajayshankar.blob.core.windows.net/bie/'
 };
