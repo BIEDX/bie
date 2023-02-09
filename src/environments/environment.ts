38308ad47ea88330125e669983fbb8ba8338e8b0
praispranav <praispranav@gmail.com>
2023-02-09 10:26:26 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://api.biedx.com',
   imgUrl: 'https://ajayshankar.blob.core.windows.net/bie/'
 };
 
