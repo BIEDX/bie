fdde054edb50f046542fbda4f5aa4fdde9e9bb32
praispranav <praispranav@gmail.com>
2023-02-09 19:20:28 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl:'https://api.biedx.com',
-  imgUrl: 'https://ajayshankar.blob.core.windows.net/bie/'
+  imgUrl: 'https://api.biedx.com/'
 };
